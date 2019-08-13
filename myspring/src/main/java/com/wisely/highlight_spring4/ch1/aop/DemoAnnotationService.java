a9213580b98f194e38c013d014f550857beb77ee
八奇 <578380312@qq.com>
2019-08-13 11:34:17 +0800

    aop

---
+++ myspring/src/main/java/com/wisely/highlight_spring4/ch1/aop/DemoAnnotationService.java
@@ -0,0 +1,9 @@
+package com.wisely.highlight_spring4.ch1.aop;
+
+public class DemoAnnotationService {
+
+    @Action(name = "注解式拦截的add操作")
+    public void add(){
+
+    }
+}
---
+++ myspring/src/main/java/com/wisely/highlight_spring4/ch1/aop/DemoMethondService.java
@@ -0,0 +1,11 @@
+package com.wisely.highlight_spring4.ch1.aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoMethondService {
+
+    public void add(){
+
+    }
+}
